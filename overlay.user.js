e85faf8bd6cf06312356f26f80022cdcb3e54ba0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-03 12:11:16 +0200

    commit

--- overlay.user.js
+++ overlay.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         r/placestart logo template
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/portalthree/place-taskbar-bot
 // @version      1
 // @description  r/placestart logo template
 // @author       portalthree
-// @match        https://hot-potato.reddit.com/embed*
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
+// @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @updateURL    https://github.com/portalthree/place-taskbar-bot/raw/main/overlay.user.js
+// @downloadURL  https://github.com/portalthree/place-taskbar-bot/raw/main/overlay.user.js
 // ==/UserScript==
 
 // credit to the osu! logo script
@@ -54,18 +56,26 @@ if (window.top !== window.self) {
         }, 100);
     }, false);
 
-    checkForUpdates();
+    setInterval(checkForUpdates, 5 * 60 * 1000);
+	await checkForUpdates();
 }
 
 function checkForUpdates(){
-    Toastify({
-        text: 'Checking for updates...',
-        duration: 10000
-    }).showToast();
-
 	fetch(`https://raw.githubusercontent.com/portalthree/place-taskbar-bot/main/version.json`, { cache: "no-store" }).then(async (response) => {
-		if (!response.ok) return console.warn('There was an error fetching the version file.');
+		if (!response.ok) return console.warn('Could not load orders!');
 		const data = await response.json();
+
+		if (JSON.stringify(data) !== JSON.stringify(placeOrders)) {
+			const structureCount = Object.keys(data.structures).length;
+			let pixelCount = 0;
+			for (const structureName in data.structures) {
+				pixelCount += data.structures[structureName].pixels.length;
+			}
+			Toastify({
+				text: `New structures loaded. Structures: ${structureCount} - Pixel: ${pixelCount}.`,
+				duration: 10000
+			}).showToast();
+		}
 
 		if (data?.version !== VERSION && !UPDATE_PENDING) {
 			UPDATE_PENDING = true
@@ -74,10 +84,11 @@ function checkForUpdates(){
 				duration: -1,
 				onClick: () => {
 					// Tapermonkey captures this and opens a new tab
-					window.location = updateURL
+					window.location = 'https://github.com/portalthree/place-taskbar-bot/raw/main/overlay.user.js'
 				}
 			}).showToast();
 
 		}
-	}).catch((e) => console.warn('Error!', e));
+		placeOrders = data;
+	}).catch((e) => console.warn('Could not load orders!', e));
 }
