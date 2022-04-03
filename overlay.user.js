848b57cac72bdadadb12dcac63a1751900cfebf0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-03 14:00:04 +0200

    update commit

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placestart logo template
 // @namespace    https://github.com/portalthree/place-taskbar-bot
-// @version      1
+// @version      2
 // @description  r/placestart logo template
 // @author       portalthree
 // @match        https://hot-potato.reddit.com/embed*
@@ -21,7 +21,9 @@
 
 const updateURL = "https://github.com/portalthree/place-taskbar-bot/raw/main/overlay.user.js";
 
-const VERSION = "1";
+const VERSION = "2";
+
+const imageLink = "https://raw.githubusercontent.com/portalthree/place-taskbar-bot/main/overlay_dotted.png";
 
 (async function () {
 
@@ -31,31 +33,30 @@ const VERSION = "1";
     if (window.top !== window.self) {    
         window.addEventListener('load', () => {
             // Load the image
-            const image = document.createElement("img");
-            image.src = "https://raw.githubusercontent.com/portalthree/place-taskbar-bot/main/overlay_dotted.png";
-            image.onload = () => {
-                image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
-            };
-    
-            // Add the image as overlay
-            const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
-            const canvas = camera.querySelector("mona-lisa-canvas");
-            canvas.shadowRoot.querySelector('.container').appendChild(image);
-    
-            // Add a style to put a hole in the pixel preview (to see the current or desired color)
-            const waitForPreview = setInterval(() => {
+                const image = document.createElement("img");
+                image.src = imageLink;
+                image.onload = () => {
+                    image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+                };
+        
+                // Add the image as overlay
+                const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
+                const canvas = camera.querySelector("mona-lisa-canvas");
+                canvas.shadowRoot.querySelector('.container').appendChild(image);
+
+                const waitForPreview = setInterval(() => {
                 const preview = camera.querySelector("mona-lisa-pixel-preview");
-                if (preview) {
-                  clearInterval(waitForPreview);
-                  const style = document.createElement('style')
-                  style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
-                  preview.shadowRoot.appendChild(style);
-                }
-            }, 100);
+                    if (preview) {
+                        clearInterval(waitForPreview);
+                        const style = document.createElement('style')
+                        style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
+                        preview.shadowRoot.appendChild(style);
+                    }
+                }, 100);
         }, false);
     }
 
-    setInterval(checkForUpdates, 5 * 60 * 1000);
+    setInterval(checkForUpdates, 1000);
 })();
 
 function Toast(text, duration) {
@@ -69,7 +70,7 @@ function Toast(text, duration) {
 
 
 function checkForUpdates(){
-    Toast("Checking for updates...", 10000);
+    console.log("Checking for updates...");
     fetch(`https://raw.githubusercontent.com/portalthree/place-taskbar-bot/main/version.json`, { cache: "no-store" }).then(async (response) => {
         if (!response.ok) return console.warn('Could not load orders!');
         const data = await response.json();
@@ -83,7 +84,8 @@ function checkForUpdates(){
                 }
             }).showToast();
 
+        } else {
+            console.log("No update found!");
         }
-        placeOrders = data;
     }).catch((e) => console.warn('Could not load orders!', e));
 }
